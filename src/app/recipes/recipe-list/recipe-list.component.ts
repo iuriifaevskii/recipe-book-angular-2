3cb7c89c96cdc2aee98021afc32cfde650c33802
Iurii Faevskii <urfaevskii@gmail.com>
2017-04-04 14:18:02 +0300

    Show ingredients list with ngFor

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -7,10 +7,11 @@ import { Recipe  } from '../recipe'
   templateUrl: './recipe-list.component.html'
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [];
+  recipes: Recipe[] = [
+    new Recipe('Schnitzel', 'Very tasty', 'http://www.daringgourmet.com/wp-content/uploads/2014/03/Schnitzel-7_edited.jpg',[]),
+    new Recipe('Summer Salad', 'Okayish', 'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/beetroot-feta-grain-salad.jpg',[])
+  ];
   @Output() recipeSelected = new EventEmitter<Recipe>();
-
-  recipe = new Recipe('Dummy', 'Dummy', 'http://a-shishkin.ru/assets/images/55gg70%20%20%202007.jpg');
 
   constructor() { }
 
--- src/app/shopping-list/shopping-list-add.component.ts
+++ src/app/shopping-list/shopping-list-add.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Ingredient } from '../shared/ingredient'
+
 @Component({
   selector: 'rb-shopping-list-add',
   templateUrl: './shopping-list-add.component.html'
 })
 export class ShoppingListAddComponent implements OnInit {
+  items: Ingredient[];
 
   constructor() { }
 
