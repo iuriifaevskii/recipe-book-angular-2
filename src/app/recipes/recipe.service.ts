ca06b4360e88498def3595c60e3272ae706fd296
Iurii Faevskii <urfaevskii@gmail.com>
2017-04-06 00:42:21 +0300

    Add cross-service communication

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@angular/core';
 
 import { Recipe } from './recipe';
+import { Ingredient } from '../shared/ingredient'
 
 @Injectable()
 export class RecipeService {
   private recipes : Recipe[] = [
-    new Recipe('Schnitzel', 'Very tasty', 'http://www.daringgourmet.com/wp-content/uploads/2014/03/Schnitzel-7_edited.jpg',[]),
+    new Recipe('Schnitzel', 'Very tasty', 'http://www.daringgourmet.com/wp-content/uploads/2014/03/Schnitzel-7_edited.jpg',[
+      new Ingredient('Franch Fries',2),
+      new Ingredient('Pork Meat Fries',1),
+    ]),
     new Recipe('Summer Salad', 'Okayish', 'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/beetroot-feta-grain-salad.jpg',[])
   ];
 
@@ -14,4 +18,7 @@ export class RecipeService {
   getRecipes(){
     return this.recipes;
   }
+
+
+
 }
